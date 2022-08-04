4e5442e0316fb2f9aa95a2f9c60765176c4ebefe
yurivalladares <yuri@valladares.com.br>
2022-08-04 09:27:11 -0300

    Update default user agent

--- medprices/settings.py
+++ medprices/settings.py
@@ -32,7 +32,7 @@ DATABASE = {
 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'medprices (http://www.example.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
